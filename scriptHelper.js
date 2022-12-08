fdd97323b1603de085239fda75789c18e458c3cf
Brad Rucker <bruck63@gmail.com>
2022-12-08 11:58:49 -0600

    fixed syntax errors

--- scriptHelper.js
+++ scriptHelper.js
@@ -25,13 +25,16 @@ function validateInput(testInput) {
     }
     //return "Not a Number"
     else if (isNaN(numberInput)){
-        return "Not a Number"
+        return "Not a Number";
     }
     //return "Is a Number"
     else if(!isNaN(numberInput)){
-        return "Is a Number"
+        return "Is a Number";
     }
 }
+// console.log(validateInput(''));
+// console.log(validateInput('Brad'));
+// console.log(validateInput('82'));
 
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
@@ -42,40 +45,41 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     let copilotStatus = document.getElementById("copilotStatus");
     let launchStatus = document.getElementById("launchStatus");
 
-    if(validateInput(pilot) === '' || validateInput(copilot) === '' || validateInput(fuelLevel) === '' || validateInput(cargoLevel) === ''){
+    if(validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty"){
         alert("All fields are required!");
     } 
-    else if(validateInput(pilot === "Is a Number") || validateInput(copilot === "Is a Number") || validateInput(fuelLevel === "Not a Number") || validateInput(cargoLevel === "Not a Number")) {
-        alert("Make sure to enter valid information for each field!")
+    else if(validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+        alert("Make sure to enter valid information for each field!");
     }
     else {
-        //list.style.visibility = "visibile"; //Turn on the faulty items div that includes the status list//
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
         copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
         if(fuelLevel < 10000 && cargoLevel > 10000){
             list.style.visibility = "visible";
             fuel.innerHTML = `Fuel level too low for launch.`;
-            cargo.innerHTML = `Cargo mass too heavy for launch.`
-            launchStatus.innerHTML = `Shuttle not ready for launch!`
+            cargo.innerHTML = `Cargo mass too heavy for launch.`;
+            launchStatus.innerHTML = `Shuttle not ready for launch!`;
             launchStatus.style.color = "red"; //Or #FF0000//
         }
         else if(fuelLevel < 10000 && cargoLevel <= 10000){
             list.style.visibility = "visible";
             fuel.innerHTML = `Fuel level too low for launch.`;
-            cargo.innerHTML = `Cargo mass low enough for launch.`
-            launchStatus.innerHTML = `Shuttle not ready for launch!`
+            cargo.innerHTML = `Cargo mass low enough for launch.`;
+            launchStatus.innerHTML = `Shuttle not ready for launch!`;
             launchStatus.style.color = "red";
         }
         else if(fuelLevel >= 10000 && cargoLevel > 10000){
             list.style.visibility = "visible";
             fuel.innerHTML = `Fuel level high enough for launch.`;
-            cargo.innerHTML = `Cargo mass too heavy for launch.`
-            launchStatus.innerHTML = `Shuttle not ready for launch!`
+            cargo.innerHTML = `Cargo mass too heavy for launch.`;
+            launchStatus.innerHTML = `Shuttle not ready for launch!`;
             launchStatus.style.color = "red";
         }
         else {
             launchStatus.innerHTML = `Shuttle is ready for launch!`
             launchStatus.style.color = "green"; //or #008000//
+            fuel.innerHTML = `Fuel level high enough for launch.`;
+            cargo.innerHTML = `Cargo mass low enough for launch.`;
         }
     }
    
